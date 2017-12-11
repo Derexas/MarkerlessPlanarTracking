5a00643d43734619eb32d291a76f25e6d4464951
derexas <derexas@gmail.com>
2017-12-11 16:49:31 +0100

    qtcreator setup

--- main.cpp
+++ main.cpp
@@ -2,16 +2,10 @@
 #include <opencv2/core/core.hpp>
 
 using namespace std;
-using namespace cv;
+
 int main()
 {
-<<<<<<< HEAD
-    cv::Mat im;
-
-=======
-
-	Mat test;
->>>>>>> 7e4d5fd36e86ed164521505e892c8e5e34834d4c
+    cv::Mat image;
     cout << "Hello World!" << endl;
     return 0;
 }
